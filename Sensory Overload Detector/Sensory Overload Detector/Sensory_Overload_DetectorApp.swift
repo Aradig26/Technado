28ac2446f520c863535c16e3a8f51cab689fbbd9
Anshu Adiga <aadiga@wpi.edu>
2025-04-11 11:57:50 -0400

    add base proj

---
+++ Sensory Overload Detector/Sensory Overload Detector/Sensory_Overload_DetectorApp.swift
@@ -0,0 +1,32 @@
+//
+//  Sensory_Overload_DetectorApp.swift
+//  Sensory Overload Detector
+//
+//  Created by Anshu Adiga on 4/11/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Sensory_Overload_DetectorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
